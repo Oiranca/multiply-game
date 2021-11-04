563bad46ccf976546dd297577c6b172a4fdaecf3
Samuel <oiranca@gmail.com>
2021-11-04 12:44:39 +0000

    wip:navbar component

---
+++ src/infrastructure/components/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import Logo from '../../../assets/img/logo.png';
+import '../NavBar/Navbar.css';
+
+export const NavBar: FC<unknown> = () => {
+  return (
+    <header className={'container-navbar'}>
+      <div className={'context-navbar'}>
+        <section className={'section-logo'}>
+          <img id={'logo'} src={Logo} alt={'Logo'} />
+        </section>
+        <section className={'section-tittle'}>
+          <h1 id={'tittle'}>Your Multiply</h1>
+        </section>
+        <section className={'section-button-navbar'}>
+          <button type={'button'} className={'navbar-button'}>
+            Sign In
+          </button>
+          <button type={'button'} className={'navbar-button'}>
+            Log In
+          </button>
+        </section>
+      </div>
+    </header>
+  );
+};
