9c7bd28152dffee561f705210a90293603183739
Samuel <oiranca@gmail.com>
2021-11-05 12:49:12 +0000

    feat: home component

--- src/infrastructure/views/Container/Container.tsx
+++ src/infrastructure/views/Container/Container.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
 import './Container.css';
 import { NavBar } from '../../components/NavBar/NavBar';
+import { Home } from '../../components/Home/Home';
 
 export const Container: FC = () => {
   return (
     <div className={'container-view'}>
-      <NavBar />
+      <header className={'header-container'}>
+        <NavBar />
+      </header>
+      <section className={'section-body'}>
+        <Home />
+      </section>
     </div>
   );
 };
