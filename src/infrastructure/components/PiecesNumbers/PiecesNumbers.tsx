cd3b6e0d283ff42e0ba19112a17598c8efa58a51
Samuel <oiranca@gmail.com>
2021-11-20 15:35:24 +0000

    feat: random position Pieces Number

--- src/infrastructure/components/PiecesNumbers/PiecesNumbers.tsx
+++ src/infrastructure/components/PiecesNumbers/PiecesNumbers.tsx
@@ -1,21 +1,35 @@
 import React, { FC, useEffect, useState } from 'react';
 import './PiecesNumbers.css';
+import { randomPosition } from '../../utils/randomPosition/randomPositionMethod';
 
-interface PropsPiecesNumber{
-  numberForMultiply:number[];
-  numberToMultiply:number;
+interface PropsPiecesNumber {
+  numberForMultiply: number[];
+  numberToMultiply: number;
 }
-export const PiecesNumbers: FC <PropsPiecesNumber>= ({numberForMultiply,numberToMultiply}) => {
+
+export const PiecesNumbers: FC<PropsPiecesNumber> = ({
+  numberForMultiply,
+  numberToMultiply
+}) => {
   const [checkOperation, setCheckOperation] = useState<boolean>(false);
   const [colorCheck, setColorCheck] = useState<string>('#e11a08');
+  const [position, setPosition] = useState<number[]>([]);
+
   useEffect(() => {
     !checkOperation ? setColorCheck('#e11a08') : setColorCheck('#33e014');
   }, [checkOperation]);
+
+  useEffect(() => {
+    setPosition(randomPosition(1, 11));
+  }, []);
+
   return (
     <article className={'table-multiply'}>
-      {numberForMultiply.map(value => (
+      {position.map(value => (
         <article className={'pieces-number'} key={value} id={`piece-${value}`}>
-          <section className={'multiply-number'}>{`${numberToMultiply}x${value}`}</section>
+          <section
+            className={'multiply-number'}
+          >{`${numberToMultiply}x${value}`}</section>
           <section className={'check-operation'} style={{ background: colorCheck }} />
         </article>
       ))}
