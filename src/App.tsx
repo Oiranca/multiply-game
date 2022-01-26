f01a81f3dbd547ba81ca6758eab6afa7d2bfbbc2
Samuel <oiranca@gmail.com>
2022-01-26 13:50:35 +0000

    wip: insert routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Container } from './infrastructure/views/Container/Container';
 import { Home } from './infrastructure/components/Home/Home';
+import { SignIn } from './infrastructure/components/SignIn/SignIn';
+import { LogIn } from './infrastructure/components/LogIn/LogIn';
+import { SelectMultiply } from './infrastructure/components/SelectMultiply/SelectMultiply';
+import { Multiply } from './infrastructure/components/Multiply/Multiply';
 
 function App() {
   return (
@@ -10,6 +14,13 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Container />}>
           <Route index element={<Home />} />
+          <Route path={'signIn'} element={<SignIn />} />
+          <Route path={'logIn'} element={<LogIn />} />
+          <Route path={'select-multiply'} element={<SelectMultiply />} />
+          <Route
+            path={'multiply:numberMultiply'}
+            element={<Multiply numberToMultiply={Number(':numberMultiply')} />}
+          />
         </Route>
       </Routes>
     </div>
--- src/infrastructure/components/NavBar/NavBar.tsx
+++ src/infrastructure/components/NavBar/NavBar.tsx
@@ -1,8 +1,22 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../../../assets/img/logo.png';
 import '../NavBar/Navbar.css';
 
 export const NavBar: FC<unknown> = () => {
+  const navigate = useNavigate();
+  const buttonSelected = (buttonId: string) => {
+    return buttonId
+      .split('-')
+      .filter(buttonName => buttonName !== 'button')
+      .toString();
+  };
+  const handleNavigation = (e: React.MouseEvent<HTMLElement>) => {
+    const buttonId = e.currentTarget.id;
+
+    navigate(buttonSelected(buttonId));
+  };
+
   return (
     <header className={'container-navbar'}>
       <div className={'context-navbar'}>
@@ -11,10 +25,10 @@ export const NavBar: FC<unknown> = () => {
           <h1 id={'tittle'}>Your Multiply</h1>
         </section>
         <section className={'section-button-navbar'}>
-          <button type={'button'} className={'navbar-button'}>
+          <button type={'button'} id={'signIn-button'} onClick={handleNavigation}>
             Sign In
           </button>
-          <button type={'button'} className={'navbar-button'}>
+          <button type={'button'} id={'logIn-button'} onClick={handleNavigation}>
             Login
           </button>
         </section>
