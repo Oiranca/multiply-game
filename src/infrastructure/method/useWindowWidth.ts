2aa430160269caf768fe83ff71303b8a2897d51b
Samuel <oiranca@gmail.com>
2022-05-15 12:20:35 +0100

    hook windows size

---
+++ src/infrastructure/method/useWindowWidth.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState }  from 'react';
+
+
+export const useWindowWidth = () => {
+  const [windowsWidth, setWindowsWith] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const listener = () => {
+      setWindowsWith(window.innerWidth);
+
+    };
+
+    window.addEventListener('resize', listener);
+
+    return () => window.removeEventListener('resize', listener);
+  }, []);
+
+  return windowsWidth;
+};
