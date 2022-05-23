2336a587a8eb07c1718550af658b1264fb7f74c4
Samuel <oiranca@gmail.com>
2022-05-23 16:08:49 +0100

    fix responsive

--- src/infrastructure/components/Multiply/MobileView/MultiplyMobile.tsx
+++ src/infrastructure/components/Multiply/MobileView/MultiplyMobile.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { randomPosition } from '../../../utils/RandomPosition/RandomPositionMethod';
-import { getOrder } from '../../../utils/OrderPosition/OrderPosition';
+import { randomPosition } from '../../../method/RandomPosition/RandomPositionMethod';
+import { getOrder } from '../../../method/OrderPosition/OrderPosition';
 import { PiecesResults } from '../../PiecesResults/PiecesResults';
 import { PiecesNumbers } from '../../PiecesNumbers/PiecesNumbers';
 
@@ -30,72 +30,76 @@ export const MultiplyMobile: FC = () => {
 
   return (
     <div className={'container-mobile'}>
-      <h1 id={'title'}> Tabla del {numberMultiply}</h1>
-      <h4 id={'information'}>
-        Selecciona un resultado y luego la multiplicación que creas correcta
-      </h4>
-      <article className={'result'}>
-        <section className={'content'}>
-          <h1 className={'title-pieces'}>Resultados</h1>
-          <section className={'piece'}>
-            {positionResults.map(value => (
-              <PiecesResults
-                key={value}
-                numberToMultiply={Number(numberMultiply)}
-                value={value}
-                onClickResult={handleSelectResult}
-              />
-            ))}
-          </section>
-        </section>
-      </article>
-      <article className={'content-table'}>
-        <h1 className={'title-pieces'}>Tabla</h1>
-        <section className={'table-piece'}>
-          <div>
-            {Object.entries(multiplyNumbers).map(value =>
-              !value[1] ? (
-                <div key={Number(value[0])} className={'pieces'}>
-                  <PiecesNumbers
-                    key={Number(value[0])}
-                    numberToMultiply={Number(numberMultiply)}
-                    value={Number(value[0])}
-                    checkOperation={false}
-                  />
-                </div>
-              ) : (
-                <div key={Number(value[0])}>
-                  <PiecesNumbers
-                    key={Number(value[0])}
-                    numberToMultiply={Number(numberMultiply)}
-                    value={Number(value[0])}
-                    checkOperation={true}
-                  />
-                </div>
-              )
-            )}
-          </div>
-
-          <div>
-            {Object.entries(resultsCorrect).map(value =>
-              !value[1] ? (
-                <div
-                  key={Number(value[0])}
-                  id={`number-index-${value[0]}`}
-                  className={`item-result`}
-                />
-              ) : (
+      <header className={'header-mobile'}>
+        <h1 id={'title'}> Tabla del {numberMultiply}</h1>
+        <h4 id={'information'}>
+          Selecciona un resultado y luego la multiplicación que creas correcta
+        </h4>
+      </header>
+      <section className={'body-mobile-container'}>
+        <article className={'result'}>
+          <section className={'content'}>
+            <h1 className={'title-pieces'}>Resultados</h1>
+            <section className={'piece'}>
+              {positionResults.map(value => (
                 <PiecesResults
-                  key={Number(value[0])}
+                  key={value}
                   numberToMultiply={Number(numberMultiply)}
-                  value={Number(value[0])}
-                  checkResult={true}
+                  value={value}
+                  onClickResult={handleSelectResult}
                 />
-              )
-            )}
-          </div>
-        </section>
-      </article>
+              ))}
+            </section>
+          </section>
+        </article>
+        <article className={'content-table'}>
+          <h1 className={'title-pieces'}>Tabla</h1>
+          <section className={'table-piece'}>
+            <div>
+              {Object.entries(multiplyNumbers).map(value =>
+                !value[1] ? (
+                  <div key={Number(value[0])} className={'pieces'}>
+                    <PiecesNumbers
+                      key={Number(value[0])}
+                      numberToMultiply={Number(numberMultiply)}
+                      value={Number(value[0])}
+                      checkOperation={false}
+                    />
+                  </div>
+                ) : (
+                  <div key={Number(value[0])}>
+                    <PiecesNumbers
+                      key={Number(value[0])}
+                      numberToMultiply={Number(numberMultiply)}
+                      value={Number(value[0])}
+                      checkOperation={true}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+
+            <div>
+              {Object.entries(resultsCorrect).map(value =>
+                !value[1] ? (
+                  <div
+                    key={Number(value[0])}
+                    id={`number-index-${value[0]}`}
+                    className={`item-result`}
+                  />
+                ) : (
+                  <PiecesResults
+                    key={Number(value[0])}
+                    numberToMultiply={Number(numberMultiply)}
+                    value={Number(value[0])}
+                    checkResult={true}
+                  />
+                )
+              )}
+            </div>
+          </section>
+        </article>
+      </section>
     </div>
   );
 };
--- src/infrastructure/components/Multiply/MultiplyIndex.tsx
+++ src/infrastructure/components/Multiply/MultiplyIndex.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 
 import './MultiplyIndex.css';
 import { MultiplyWeb } from './MultiplyWeb/MultiplyWeb';
-import { useWindowWidth } from '../../method/useWindowWidth';
+import { useWindowWidth } from '../../method/useWindowsWidth/useWindowWidth';
 import { MultiplyMobile } from './MobileView/MultiplyMobile';
 
 export const MultiplyIndex: FC = () => {
   return (
     <div className={'multiply-content'}>
-      {useWindowWidth() > 640 ? <MultiplyWeb /> : <MultiplyMobile />}
+      {useWindowWidth() >= 820 ? <MultiplyWeb /> : <MultiplyMobile />}
     </div>
   );
 };
