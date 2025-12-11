7a2fe61a9289a4e6697b0b0a8a9bd97cefaf42bd
Samuel Romero Arbelo <samuelromeroarbelo@gmail.com>
2025-12-11 10:24:08 +0000

    feat: implement Login component with user authentication and guest login functionality

---
+++ src/test/vitest.d.ts
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { expect } from 'vitest';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends jest.Matchers<void, T> {}
+  interface AsymmetricMatchersContaining extends jest.Matchers<void, any> {}
+}
+
